1898507ad22cc1828f9427957cf7a3e0b1e983df
Dmitrij <s.d144@yandex.by>
2020-10-03 19:18:12 +0300

    init: start typical-array-problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length === 0) {
+    return 0
+  }
+  let smallest = array[0]
+  for (i = 1; i <= array.length; i++) {
+    if (array[i] < smallest) {
+      smallest = array[i]
+    }
+  }
+  return smallest
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length === 0) {
+    return 0
+  }
+  let biggest = array[0]
+  for (i = 1; i <= array.length; i++) {
+    if (array[i] > biggest) {
+      biggest = array[i]
+    }
+  }
+  return biggest
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {
+    return 0
+  }
+  let sum = 0;
+  for (i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum / array.length
 }
